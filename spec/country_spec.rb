286a7afd91b5cc3e7fcad7d34905b2cb2d3b3f87
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2015-06-10 12:48:24 +0300

    Top-level country class removed

--- spec/country_spec.rb
+++ spec/country_spec.rb
@@ -294,28 +294,6 @@ describe ISO3166::Country do
 
       it { is_expected.to be_falsey }
     end
-
-    context "when loaded via 'countries'" do
-      before { require 'countries' }
-
-      describe 'existance' do
-        subject { defined?(Country) }
-
-        it { is_expected.to be_truthy }
-      end
-
-      describe 'superclass' do
-        subject { Country.superclass }
-
-        it { is_expected.to eq(ISO3166::Country) }
-      end
-
-      describe 'to_s' do
-        it 'should return the country name' do
-          expect(Country.new('GB').to_s).to eq('United Kingdom')
-        end
-      end
-    end
   end
 
   describe 'find_all_by' do
